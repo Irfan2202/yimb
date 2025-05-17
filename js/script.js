ed76f260efd4051f6bbf4b47e85a40c8d5dc4687
Irfan2202 <112533797+Irfan2202@users.noreply.github.com>
2025-05-17 16:00:41 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(".program .owl-carousel").owlCarousel({
+  dots: false,
+  loop: false,
+  center: false,
+  margin: 10,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 2,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
